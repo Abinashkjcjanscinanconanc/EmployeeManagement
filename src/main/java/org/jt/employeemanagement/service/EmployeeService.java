fe676750ddc428418a8993b7939804d4b267b5a2
Abinash <swainabinash36@gmail.com>
2023-06-09 09:40:00 +0530

    update

---
+++ src/main/java/org/jt/employeemanagement/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package org.jt.employeemanagement.service;
+
+import java.util.List;
+
+import org.jt.employeemanagement.model.Employee;
+
+public interface EmployeeService {
+    /**
+     * <h1>Save Employee in to DB</h1>
+     * <p>
+     * This method take employee object
+     * as parameter and save it into Database
+     * </p>
+     * 
+     * @param org.jt.employeemanagement.model.Employee
+     */
+    void saveEmployee(Employee employee);
+
+    List<Employee> getEmployees();
+}
---
+++ src/main/java/org/jt/employeemanagement/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package org.jt.employeemanagement.service.impl;
+
+import java.util.List;
+
+import org.jt.employeemanagement.model.Employee;
+import org.jt.employeemanagement.repository.EmployeeRepository;
+import org.jt.employeemanagement.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+}
